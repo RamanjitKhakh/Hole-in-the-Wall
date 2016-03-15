7d6bd6643510ab74f3fe0a85a0e4c858649aba71
adeutry <adeutry@temple.edu>
2016-03-14 21:35:08 -0400

    init commit

---
+++ src/mygame/Main.java
@@ -0,0 +1,149 @@
+package mygame;
+
+/**
+ * This is a template that provides a start setup for many projects. It provides
+ * initialization of all major components: initAppScreen(app); initGui();
+ * initMaterials(); initLightandShadow(); initGeometries(); initCam();
+ */
+import com.jme3.app.SimpleApplication;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.queue.RenderQueue.ShadowMode;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Sphere;
+import com.jme3.shadow.DirectionalLightShadowRenderer;
+import com.jme3.system.AppSettings;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import poormocapplayer.MocapPlayer;
+
+public class Main extends SimpleApplication {
+
+    public static Material gold, magenta;
+    Geometry geomSphere, geomBox;
+    MocapPlayer player;
+    Skeleton[] skeletons;
+
+    public static void main(String[] args) {
+        Main app = new Main();
+        initAppScreen(app);
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        player = new MocapPlayer("assets/etc/Test3.serial");
+        initGui();
+        initMaterials();
+        initLightandShadow();
+        initGeometries();
+        initCam();
+        initSkeletons();
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        int[][] joints = player.getJoints();
+        if (joints != null) {
+            for (Skeleton skeleton : skeletons) {
+                skeleton.setJoints(player.getJoints());
+                skeleton.draw();
+            }
+        }
+    }
+
+    // -------------------------------------------------------------------------
+    // Initialization Methods
+    // -------------------------------------------------------------------------
+    private static void initAppScreen(SimpleApplication app) {
+        AppSettings aps = new AppSettings(true);
+        Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        screen.width *= 0.75;
+        screen.height *= 0.75;
+        aps.setResolution(screen.width, screen.height);
+        app.setSettings(aps);
+        app.setShowSettings(false);
+    }
+
+    private void initMaterials() {
+        gold = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
+        gold.setBoolean("UseMaterialColors", true);
+        gold.setColor("Ambient", ColorRGBA.Red);
+        gold.setColor("Diffuse", ColorRGBA.Green);
+        gold.setColor("Specular", ColorRGBA.Gray);
+        gold.setFloat("Shininess", 4f); // shininess from 1-128
+
+        magenta = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
+        magenta.setBoolean("UseMaterialColors", true);
+        magenta.setColor("Ambient", ColorRGBA.Gray);
+        magenta.setColor("Diffuse", ColorRGBA.Blue);
+        magenta.setColor("Specular", ColorRGBA.Red);
+        magenta.setFloat("Shininess", 2f); // shininess from 1-128
+    }
+
+    private void initGui() {
+        setDisplayFps(true);
+        setDisplayStatView(false);
+    }
+
+    private void initLightandShadow() {
+        // Light1: white, directional
+        DirectionalLight sun = new DirectionalLight();
+        sun.setDirection((new Vector3f(-0.7f, -1.3f, -0.9f)).normalizeLocal());
+        sun.setColor(ColorRGBA.Gray);
+        rootNode.addLight(sun);
+
+        // Light 2: Ambient, gray
+        AmbientLight ambient = new AmbientLight();
+        ambient.setColor(new ColorRGBA(0.7f, 0.7f, 0.7f, 1.0f));
+        rootNode.addLight(ambient);
+
+        // SHADOW
+        // the second parameter is the resolution. Experiment with it! (Must be a power of 2)
+        DirectionalLightShadowRenderer dlsr = new DirectionalLightShadowRenderer(assetManager, 512, 1);
+        dlsr.setLight(sun);
+        viewPort.addProcessor(dlsr);
+    }
+
+    private void initGeometries() {
+        // Materials must be initialized first
+        // Large Sphere
+        Sphere sphereLarge = new Sphere(32, 32, 1.5f);
+        geomSphere = new Geometry("Shiny", sphereLarge);
+        geomSphere.setMaterial(gold);
+        geomSphere.setLocalTranslation(0, 2f, 0);
+        //rootNode.attachChild(geomSphere);
+
+        // Ground
+        Box box = new Box(8f, 2f, 8f);
+        geomBox = new Geometry("ground", box);
+        geomBox.setMaterial(magenta);
+        geomBox.setLocalTranslation(0, -4f, 0);
+        rootNode.attachChild(geomBox);
+
+        // define shadow behavior
+        geomSphere.setShadowMode(ShadowMode.CastAndReceive);
+        geomBox.setShadowMode(ShadowMode.Receive);
+    }
+
+    private void initCam() {
+        flyCam.setEnabled(true);
+        cam.setLocation(new Vector3f(1f, 3f, 10f));
+        cam.lookAt(Vector3f.ZERO, Vector3f.UNIT_Y);
+    }
+
+    private void initSkeletons() {
+        skeletons = new Skeleton[16];
+        for (int i = 0; i < skeletons.length; i++) {
+            int indexX = i%4;
+            int indexZ = i/4;
+            skeletons[i] = new Skeleton(this);
+            skeletons[i].setLocalTranslation(indexX * 2f - 3, -1, indexZ*2f - 3);
+            rootNode.attachChild(skeletons[i]);
+        }
+    }
+}
---
+++ src/mygame/Skeleton.java
@@ -0,0 +1,135 @@
+package mygame;
+
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.shape.Cylinder;
+import com.jme3.scene.shape.Sphere;
+
+/**
+ *
+ * @author rolf
+ */
+public class Skeleton extends Node {
+
+    Main main;
+    // the skeleton joints.
+    // they contain the 3d positions, converted from kinect system (mm) to JMonkey units (meters)
+    // the joints contain:  [0]-[2]: x,y,z 
+    // Index order:
+    //0.HipCenter
+    //1.Spine
+    //2.ShoulderCenter
+    //3.Head
+    //4.ShoulderLeft
+    //5.ElbowLeft
+    //6.WristLeft
+    //7.HandLeft
+    //8.ShoulderRight
+    //9.ElbowRight
+    //10.WristRight
+    //11.HandRight
+    //12.HipLeft
+    //13.KneeLeft
+    //14.AnkleLeft
+    //15.FootLeft
+    //16.HipRight
+    //17.KneeRight
+    //18.AnkleRight
+    //19.FootRight 
+    float[][] joints;
+    // bones connect joints
+    Geometry[] bones;
+    Geometry head;
+    // the jointIndices define the skeleton, they are pairs of
+    // joint indices which are connected by bones
+    int[][] jointIndices = {{3, 2}, {2, 1}, {1, 0},
+        {2, 8}, {8, 9}, {9, 10}, {10, 11},
+        {2, 4}, {4, 5}, {5, 6}, {6, 7},
+        {0, 16}, {16, 17}, {17, 18}, {18, 19},
+        {0, 12}, {12, 13}, {13, 14}, {14, 15}};
+
+    // Constructor
+    public Skeleton(Main main) {
+        this.main = main;
+        joints = new float[20][3];
+        bones = new Geometry[jointIndices.length];
+        initBones();
+    }
+
+    // copy array of joint positions into joint-array
+    // the input array is different from the joint array! it
+    // is in microsoft kinect format, i.e. the position data is in
+    // [1] - [3]. Also, it is in mm, and the z-axis points away from the viewer.
+    public void setJoints(int[][] j) {
+        for (int i = 0; i < j.length; i++) {
+            joints[i][0] = (float)j[i][1]*0.001f;
+            joints[i][1] = (float)j[i][2]*0.001f;
+            joints[i][2] = -(float)j[i][3]*0.001f;
+        }
+    }
+
+    public void draw() {
+        for (int i = 0; i < bones.length; i++) {
+            transformCylinder(i);
+        }
+        head.setLocalTranslation(joints[3][0],joints[3][1],joints[3][2]);
+    }
+
+    private void transformCylinder(int cylinderIndex) {
+        // get start and end
+        int istart = jointIndices[cylinderIndex][0];
+        int iend = jointIndices[cylinderIndex][1];
+        Vector3f start = new Vector3f(joints[istart][0], joints[istart][1], joints[istart][2]);
+        Vector3f end = new Vector3f(joints[iend][0], joints[iend][1], joints[iend][2]);
+
+
+        // scale, rotate and translate the cylinder
+        // scale: in Z direction, by length (end-start)
+        float length = end.distance(start);
+        bones[cylinderIndex].setLocalScale(1f, 1f, length);
+
+        // rotation using quaternions
+        // target direction dir = end-start
+        // axis: dir x (0 0 1)
+        // angle: acos (dir/|dir| o (0 0 1)) = acos((dir/|dir|).z)
+        Vector3f dir = end.subtract(start);
+        dir.normalizeLocal();
+        Vector3f axis = dir.cross(Vector3f.UNIT_Z);
+        float sin = axis.length();
+        float angle = -FastMath.atan2(sin, dir.z);
+
+        Quaternion q = new Quaternion();
+        q.fromAngleAxis(angle, axis);
+        bones[cylinderIndex].setLocalRotation(q);
+
+        // translate: centerpoint between start and end is translation vector
+        Vector3f center = start.add(end);
+        center.multLocal(0.5f);
+        bones[cylinderIndex].setLocalTranslation(center);
+    }
+
+    private void initBones() {
+        Cylinder c = new Cylinder(10, 10, 0.08f, 1f, true);
+        Material matC = new Material(main.getAssetManager(), "Common/MatDefs/Light/Lighting.j3md");
+        matC.setBoolean("UseMaterialColors", true);
+        matC.setColor("Ambient", ColorRGBA.Gray);
+        matC.setColor("Diffuse", ColorRGBA.Yellow);
+        matC.setColor("Specular", ColorRGBA.Red);
+        matC.setFloat("Shininess", 2f); // shininess from 1-128
+        for (int i = 0; i < bones.length; i++) {
+            bones[i] = new Geometry("", c);
+            bones[i].setMaterial(matC);
+            attachChild(bones[i]);
+        }
+        
+        Sphere s = new Sphere(10,10,0.2f);
+        head = new Geometry("", s);
+        head.setMaterial(matC);
+        attachChild(head);
+    }
+}
